74db3166625e46e6332b0c3ee65f2af522798300
Brady Stephenson <brmstephenson@gmail.com>
2017-03-29 20:33:15 -0400

    Added github pages

--- src/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import angular from 'angular';
-
-import './components/modules/modules';
-import './components/grid-service/grid-service';
-import './components/rules-service/rules-service';
-import './components/gameboard/gameboard';
